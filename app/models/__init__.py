29a01706f13ba2f2f59433ee135080c38b7cb3b9
linlin7508 <dastin0505@gmail.com>
2026-06-10 02:04:06 +0800

    Import all models for create_all

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,8 @@ from app.models.user import User
 from app.models.arena import Arena, Battle
 from app.models.creature import Creature, ElementType
 from app.models.achievement import Achievement, UserAchievement
-from app.models.bus import BusRoute
+from app.models.bus import BusPosition, BusRoute, BusRouteShape, BusStop
 from app.models.chat import ChatMessage, UserMemory
+from app.models.friend import Friend
+from app.models.profile import Profile
+from app.models.shop import Inventory, Item
